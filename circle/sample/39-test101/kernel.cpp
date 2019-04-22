89d6741696e23a46c8512cf73e4233d4c27eabf5
Abduarahman Bazena <abduarahman.bazena@gmail.com>
2019-04-22 11:05:41 +0200

    wip

--- circle/sample/39-test101/kernel.cpp
+++ circle/sample/39-test101/kernel.cpp
@@ -22,12 +22,27 @@
 #include <circle/timer.h>
 #define GPIOPinOne 19
 
-CGPIOPin MasterEnable (15 , GPIOModeOutput);
-CGPIOPin EnableOne (2 , GPIOModeOutput);
-CGPIOPin EnableTwo (17 , GPIOModeOutput);
-CGPIOPin InputOne (3 , GPIOModeOutput);
-CGPIOPin InputTwo (4 , GPIOModeOutput);
-CGPIOPin InputThree (14 , GPIOModeOutput);
+CGPIOPin DecoderMasterEnable (15 , GPIOModeOutput);
+CGPIOPin OutputMasterEnable (11 , GPIOModeOutput);
+CGPIOPin DecoderEnableOne (2 , GPIOModeOutput);
+CGPIOPin DecoderEnableTwo (17 , GPIOModeOutput);
+CGPIOPin DecoderPins[] =
+						{
+							CGPIOPin (3 , GPIOModeOutput),
+							CGPIOPin (4 , GPIOModeOutput),
+							CGPIOPin (14 , GPIOModeOutput)
+						};
+CGPIOPin OutputPins[] =
+						{
+							CGPIOPin (18 , GPIOModeOutput),
+							CGPIOPin (27 , GPIOModeOutput),
+							CGPIOPin (22 , GPIOModeOutput),
+							CGPIOPin (23 , GPIOModeOutput),
+							CGPIOPin (24 , GPIOModeOutput),
+							CGPIOPin (10 , GPIOModeOutput),
+							CGPIOPin (9 , GPIOModeOutput),
+							CGPIOPin (15 , GPIOModeOutput)
+						};
 
 CKernel::CKernel (void)
 {
@@ -39,57 +54,65 @@ CKernel::~CKernel (void)
 
 boolean CKernel::Initialize (void)
 {
+	DecoderEnableOne.Write(0);
+	DecoderEnableTwo.Write(0);
 	return TRUE;
 }
-int UpdatePulse(int output)
+int UpdateOutput(byte output[8])
+{
+	for (int i = 0; i++; i<8)
+	{
+		OutputPins[i].Write(output[i]);
+  }
+}
+int UpdatePulse(unsigned output)
 {
 	if(output == 1)
 	{
+		DecoderPins[0].Write(0);
+		DecoderPins[1].Write(0);
+		DecoderPins[2].Write(0);
 		MasterEnable.Write(1);
-		EnableOne.Write(0);
-		EnableTwo.Write(0);
-		InputOne.Write(0);
-		InputTwo.Write(0);
-		InputThree.Write(0);
+
 	}
 	else if(output == 2)
 	{
+		DecoderPins[0].Write(1);
+		DecoderPins[1].Write(0);
+		DecoderPins[2].Write(0);
 		MasterEnable.Write(1);
-		EnableOne.Write(0);
-		EnableTwo.Write(0);
-		InputOne.Write(1);
-		InputTwo.Write(0);
-		InputThree.Write(0);
 	}
 	else
 	{
+		DecoderPins[0].Write(0);
+		DecoderPins[1].Write(1);
+		DecoderPins[2].Write(0);
 		MasterEnable.Write(1);
-		EnableOne.Write(0);
-		EnableTwo.Write(0);
-		InputOne.Write(0);
-		InputTwo.Write(1);
-		InputThree.Write(0);
 	}
 	return 0;
 }
 
 TShutdownMode CKernel::Run (void)
 {
-	CGPIOPin AudioLeft (GPIOPinAudioLeft, GPIOModeOutput);
-	CGPIOPin AudioRight (GPIOPinAudioRight, GPIOModeOutput);
-	CGPIOPin GPIOOne (GPIOPinOne , GPIOModeOutput);
+	//CGPIOPin AudioLeft (GPIOPinAudioLeft, GPIOModeOutput);
+	//CGPIOPin AudioRight (GPIOPinAudioRight, GPIOModeOutput);
+	//CGPIOPin GPIOOne (GPIOPinOne , GPIOModeOutput);
 	// flash the Act LED 10 times and click on audio (3.5mm headphone jack)
 	for (unsigned i = 1; i <= 1000; i++)
 	{
+		UpdateOutput( {0,0,0,0,0,0,0,0} );
 		UpdatePulse(1);
-		GPIOOne.Write(1);
-		m_ActLED.On ();
-		CTimer::SimpleMsDelay (5000);
-		GPIOOne.Write(0);
-		m_ActLED.Off ();
-		AudioLeft.Invert ();
-		AudioRight.Invert ();
-		CTimer::SimpleMsDelay (5000);
+
+
+
+		//GPIOOne.Write(1);
+		//m_ActLED.On ();
+		//CTimer::SimpleMsDelay (5000);
+		//GPIOOne.Write(0);
+		//m_ActLED.Off ();
+		//AudioLeft.Invert ();
+		//AudioRight.Invert ();
+		//CTimer::SimpleMsDelay (5000);
 	}
 
 	return ShutdownReboot;
